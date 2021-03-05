3eb551e49979c425d2eda4295b35e086dea84739
Sean Barag <sean@barag.org>
2021-03-05 07:32:59 -0800

    doc: add docs for try_set_profile

--- thcon.py
+++ thcon.py
@@ -2,24 +2,35 @@
 import asyncio
 import json
 import iterm2
-import time
 import sys
 
 __DEBUG = True
 
 async def try_set_profile(connection, app, payload):
+    """
+    Attempts to find an iTerm2 profile matching the name included in the payload,
+    then uses that profile to change all sessions in all tabs in all windows and
+    sets the default profile for any sessions made later.
+
+    :param iterm2.Connection connection: a websocket connection to the iTerm2 backend
+    :param iterm2.App app: a reference to the currently-running iTerm2 application
+    :param dict payload: a dictionary matching ./thcon.schema.json
+    """
     if not "profile" in payload:
-        print("Couldn't find 'profile' property in payload: ", payload)
+        print("WARNING: Couldn't find 'profile' property in payload: ", payload)
         return
 
     profiles = await iterm2.PartialProfile.async_query(connection)
     matching_profiles = [ p for p in profiles if p.name == payload["profile"] ]
     if not matching_profiles:
+        print("WARNING: Couldn't find profile named '{}'".format(payload["profile"]))
         return
 
     profile = await matching_profiles[0].async_get_full_profile()
     if profile is None:
+        print("ERROR: Couldn't get full profile named '{}'".format(payload["profile"]))
         return
+
     futures = []
     # gather all sessions...
     for window in app.windows:
